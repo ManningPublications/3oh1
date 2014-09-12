91767060ab316e10c03958ab7cf475650d37f753
tom <toea@manning.com>
2014-09-12 16:45:28 -0700

    Added qa datasource (doh!)

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -23,6 +23,12 @@ environments {
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
+    qa {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
     test {
         dataSource {
             dbCreate = "update"
