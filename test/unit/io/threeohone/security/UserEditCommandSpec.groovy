8c593c773b63722464117496d33b86d321b5b455
bresche <csh.shop@gmail.com>
2014-10-30 20:17:10 +0100

    [#31] (+) unit test for command object

---
+++ test/unit/io/threeohone/security/UserEditCommandSpec.groovy
@@ -0,0 +1,56 @@
+package io.threeohone.security
+
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.Specification
+
+@TestMixin(GrailsUnitTestMixin)
+class UserEditCommandSpec extends Specification {
+
+    def userEditCommand = new UserEditCommand(version: 0, username: 'username')
+
+
+
+    def 'password and confirmPassword is the same'() {
+        when:
+        userEditCommand.password = 'password'
+        userEditCommand.confirmPassword = 'password'
+        userEditCommand.validate()
+
+        then:
+        !userEditCommand.hasErrors()
+    }
+
+    def 'different passwords'() {
+        when:
+        userEditCommand.password = 'password'
+        userEditCommand.confirmPassword = 'noTheSame'
+        userEditCommand.validate()
+
+        then:
+        userEditCommand.hasErrors()
+        userEditCommand.errors['password'].code == 'invalid.matchingpasswords'
+    }
+
+    def 'confirmPassword can not be blank'() {
+        when:
+        userEditCommand.password = 'password'
+        userEditCommand.confirmPassword = ''
+        userEditCommand.validate()
+
+        then:
+        userEditCommand.hasErrors()
+        userEditCommand.errors['confirmPassword'].code == 'blank'
+    }
+
+    def 'password can not be blank'() {
+        when:
+        userEditCommand.password = ''
+        userEditCommand.confirmPassword = 'password'
+        userEditCommand.validate()
+
+        then:
+        userEditCommand.hasErrors()
+        userEditCommand.errors['password'].code == 'blank'
+    }
+}
