8aed6f58e035d8eb3e4ae4854118b98a743e8736
Mario David <mariodavid@arcor.de>
2016-04-02 11:44:51 +0200

    added postgres hibernate sequence

---
+++ grails-app/migrations/changelog-hibernate-sequence-postgres.groovy
@@ -0,0 +1,5 @@
+databaseChangeLog = {
+    changeSet(author: "mario (generated)", id: "1416120497231-30", dbms: "postgres") {
+        createSequence(sequenceName: "hibernate_sequence")
+    }
+}
--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,3 +1,4 @@
 databaseChangeLog = {
     include file: 'changelog-1-0.groovy'
+    include file: 'changelog-hibernate-sequence-postgres.groovy'
 }
