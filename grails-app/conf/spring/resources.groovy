a722397da423e5722511c84aaf22ea7fa6522e1a
amr <amr@amr-laptop>
2018-05-24 09:55:12 +0200

    basic auth config

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,2 +1,9 @@
+import org.springframework.security.web.context.NullSecurityContextRepository
+import org.springframework.security.web.context.SecurityContextPersistenceFilter
+
 beans = {
+
+    statelessSecurityContextRepository(NullSecurityContextRepository)
+
+    statelessSecurityContextPersistenceFilter(SecurityContextPersistenceFilter, ref('statelessSecurityContextRepository'))
 }
