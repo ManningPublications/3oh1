44b3ef85344ae16cc53ccc9d30810cc7c3455baf
amr <a.mas.rodriguez@gmail.com>
2018-06-13 20:35:08 +0200

    Add city dictionary for go locations fix redirectLoggingService

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,7 +1,10 @@
+import com.maxmind.geoip.LookupService
 import org.springframework.security.web.context.NullSecurityContextRepository
 import org.springframework.security.web.context.SecurityContextPersistenceFilter
 
 beans = {
     statelessSecurityContextRepository(NullSecurityContextRepository)
     statelessSecurityContextPersistenceFilter(SecurityContextPersistenceFilter, ref('statelessSecurityContextRepository'))
+
+    geoIpService(LookupService, this.class.getClassLoader().getResource(grailsApplication.config.grails.plugin.geoip.data.path as String).file)
 }
